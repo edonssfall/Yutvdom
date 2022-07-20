d8a42aeefe0df2259ea6fec51c8a9ff12d1764cf
Leonid Tsarov <edonssfall123@gmail.com>
2022-07-20 13:04:42 +0200

    1

--- yutvdom_test/apps/authentication/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import sign_up
-
-app_name = 'apps.authentication'
-
-urlpatterns = [
-    path("", sign_up)
-]
--- yutvdom_test/config/settings/settings.py
+++ yutvdom_test/config/settings/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from secret import secret_key, database
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(q2+03ru+r@q29w*f-p9b@xd52%e9ioh(a1!=+guwqxb6_7b@a'
+SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.authentication.apps.AuthenticationConfig'
 ]
 
 MIDDLEWARE = [
@@ -73,12 +75,7 @@ WSGI_APPLICATION = 'yutvdom.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = database
 
 
 # Password validation
